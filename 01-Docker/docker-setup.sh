245483d52f5d1dce8598287d6feee7ee0b895e45
Ayo-milua <ayomide@lbcit.ca>
2026-01-06 23:31:24 -0500

    Day 1: docker commands and Dockerfile revisions

---
+++ 01-Docker/docker-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Install Docker on Amazon Linux 2023
+
+sudo dnf update -y
+sudo dnf install docker -y
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
